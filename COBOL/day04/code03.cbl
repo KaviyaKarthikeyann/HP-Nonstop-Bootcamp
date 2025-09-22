23867ef14e0077138e127c6f6dbde637b2d284cc
Kaviya Karthikeyan <kaviya.e0219062@sret.edu.in>
2025-09-22 13:27:27 -0400

    pic , sign clause

--- COBOL/day04/code03.cbl
+++ COBOL/day04/code03.cbl
@@ -1,19 +1,27 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. mynum.
+       PROGRAM-ID. mydetails.
        environment DIVISION.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       01  my-name pic x(20).
+       01  my-name pic x(20)
        01  my-num  PIC 9(10).
+       01  my-income pic 9(3)v9(5).
+       01  my-tax pic 9(2)v9(2).
+       01 my-marks pic s99.
   
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
            MOVE "KAVIYA KARTHIKEYAN" TO my-name.
            MOVE 9857753948 TO my-num.
+           move -20 to my-marks.
+           move 100.56437 to my-income.
+           move 20.20 to my-tax.
             DISPLAY my-name
             DISPLAY my-num
+            display my-income
+            display my-marks.
+            display my-tax.
 
             STOP RUN.
-       END PROGRAM mynum.
-
+       END PROGRAM mydetails.
