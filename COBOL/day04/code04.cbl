b5679f0b214c4b631e4216066fcd45df53c24c11
Kaviya Karthikeyan <kaviya.e0219062@sret.edu.in>
2025-09-22 14:51:47 -0400

    value clause

--- COBOL/day04/code04.cbl
+++ COBOL/day04/code04.cbl
@@ -7,16 +7,16 @@
        01  my-name pic x(20).
        01  my-num  PIC 9(10).
        01  my-income pic z,z9.9(2).
-       01  my-tax pic 9(2)v9(2).
-       01 my-marks pic s99.
+       01  my-tax pic 9(2)v9(2) value zeros.
+       01 my-marks pic s99 value spaces
   
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
            MOVE "KAVIYA KARTHIKEYAN" TO my-name.
            MOVE 9857753948 TO my-num.
-           move -20 to my-marks.
+           *move -20 to my-marks.
            move 100.56437 to my-income.
-           move 20.20 to my-tax.
+           *move 20.20 to my-tax.
             DISPLAY my-name
             DISPLAY my-num
             display my-income
