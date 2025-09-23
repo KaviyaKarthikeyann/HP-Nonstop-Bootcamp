b1d66e5ee2e915bb7e2bc6285d3645f94cf9db60
Kaviya Karthikeyan <kaviya.e0219062@sret.edu.in>
2025-09-23 15:36:45 -0400

    move

--- COBOL/day05/code1.cbl
+++ COBOL/day05/code1.cbl
@@ -4,9 +4,12 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
        01  phone pic 9(10).
+       01 myphone pic 9(10).
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
            move 8434348973 to phone
+           move phone to myphone
+           display "my phone is " myphone
             DISPLAY phone
             STOP RUN.
        END PROGRAM move.
