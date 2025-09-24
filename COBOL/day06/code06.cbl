1ff23e1bcdb2e47dd1bae2cb92bddf6eb7efdb1d
Kaviya Karthikeyan <kaviya.e0219062@sret.edu.in>
2025-09-24 15:25:30 -0400

    functions

--- COBOL/day06/code06.cbl
+++ COBOL/day06/code06.cbl
@@ -1,5 +1,5 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. maxi.
+       PROGRAM-ID. func.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
@@ -7,5 +7,11 @@
        MAIN-PROCEDURE.
             DISPLAY "max is " function max(12 37 373 27973 439938).
             DISPLAY "min is " function min(12 37 373 27973 439938).
+            DISPLAY "mean is " function mean(12 37 373 27973 439938).
+            DISPLAY "sum is " function sum(12 37 373 27973 439938).         
+            DISPLAY "sqrt of 439938 is " function sqrt(439938) rounded .
+            DISPLAY "length is " function length("kaviya").
+            DISPLAY "sin of 12 is " function sin(12).
+            DISPLAY "cos of 12 is " function cos(12).
             STOP RUN.
-       END PROGRAM maxi.
+       END PROGRAM func.
