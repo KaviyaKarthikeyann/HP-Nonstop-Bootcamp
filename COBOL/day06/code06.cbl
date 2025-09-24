0c2f65f9253f660c36adf9d872a98e0c62c2eac6
Kaviya Karthikeyan <kaviya.e0219062@sret.edu.in>
2025-09-24 15:27:59 -0400

    functions

--- COBOL/day06/code06.cbl
+++ COBOL/day06/code06.cbl
@@ -9,8 +9,9 @@
             DISPLAY "min is " function min(12 37 373 27973 439938).
             DISPLAY "mean is " function mean(12 37 373 27973 439938).
             DISPLAY "sum is " function sum(12 37 373 27973 439938).         
-            DISPLAY "sqrt of 439938 is " function sqrt(439938) rounded .
+            DISPLAY "sqrt of 439938 is " function sqrt(439938)  .
             DISPLAY "length is " function length("kaviya").
+            display "reverse is " function reverse("car").
             DISPLAY "sin of 12 is " function sin(12).
             DISPLAY "cos of 12 is " function cos(12).
             STOP RUN.
