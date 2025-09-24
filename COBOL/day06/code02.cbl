19c34ffd0dd538254e811372c52d7d3172a87d31
Kaviya Karthikeyan <kaviya.e0219062@sret.edu.in>
2025-09-24 11:40:11 -0400

    div

--- COBOL/day06/code02.cbl
+++ COBOL/day06/code02.cbl
@@ -1,14 +1,19 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. div.
+       PROGRAM-ID. divi.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
        01  a pic 99 value 24.
        01  b pic 99. 
+       01  c pic 99.
+       01  d pic 99.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
            divide 2 into a giving b.
             DISPLAY b.
+            DIVIDE a by 5 GIVING c.
+            DISPLAY "c is " c.
             STOP RUN.
-       END PROGRAM div.
+       END PROGRAM divi.
+
 
