85722880834bdc4b3a61dfa22b45ccb287d52c92
Kaviya Karthikeyan <kaviya.e0219062@sret.edu.in>
2025-09-24 15:17:30 -0400

    max and min function

--- COBOL/day06/code06.cbl
+++ COBOL/day06/code06.cbl
@@ -5,6 +5,7 @@
        WORKING-STORAGE SECTION.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-            DISPLAY function max(12 37 373 27973 439938)
+            DISPLAY "max is " function max(12 37 373 27973 439938).
+            DISPLAY "min is " function min(12 37 373 27973 439938).
             STOP RUN.
        END PROGRAM maxi.
