b641e49ad3a76670887789af5ba94cbfda950009
Kaviya Karthikeyan <kaviya.e0219062@sret.edu.in>
2025-09-24 11:07:36 -0400

    multiply

--- COBOL/day05/code06.cbl
+++ COBOL/day05/code06.cbl
@@ -3,15 +3,16 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       01  a pic 9(4) VALUE 1000.
-       01  b pic 9(1) value 1.
+       01  a pic 9(3) VALUE 10.
+       01  b pic 9(3) value 10.
        01  c pic 9(3) value ZEROES.
        01  answer pic 9(3) .       
        
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-           subtract b from a GIVING c.
-           compute answer = a - b.
+           multiply a by b 
+            
+           
            display "answer " answer.
            
             DISPLAY a.
@@ -19,4 +20,5 @@
             display c.
             STOP RUN.
        END PROGRAM sub.
+
 
