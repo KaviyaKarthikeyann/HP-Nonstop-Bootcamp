3be2061b947a30b16bd2360cd357839949f3a601
Kaviya Karthikeyan <kaviya.e0219062@sret.edu.in>
2025-09-24 12:11:33 -0400

    expo

---
+++ COBOL/day06/code03.cbl
@@ -0,0 +1,16 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. expo.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       01  a pic 9 value 2.
+       01  b pic 9 value 2.
+       01  c pic 9.
+       
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           compute c= a**b.
+            DISPLAY c.
+            STOP RUN.
+       END PROGRAM expo.
+
