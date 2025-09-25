18806390affe0c9ebb9f8597feb0e72ee4dac7a2
Kaviya Karthikeyan <kaviya.e0219062@sret.edu.in>
2025-09-25 12:53:53 -0400

    present value

---
+++ COBOL/day07/code03.cbl
@@ -0,0 +1,18 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           display FUNCTION PRESENT-VALUE(.5 200 200 200 200) .
+            
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
+
